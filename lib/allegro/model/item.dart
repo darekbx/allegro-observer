99a79e80de2a17a05ad63b85a158f97cedadcbd6
Darek-Mac <darek.baranczuk@gmail.com>
2018-09-19 22:22:26 +0200

    Allegro listings, full model

--- lib/allegro/model/item.dart
+++ lib/allegro/model/item.dart
@@ -1,14 +1,28 @@
+import 'image.dart';
+import 'parameter.dart';
+import 'sellingmode.dart';
 
 class Item {
 
   final String id;
   final String name;
   final String url;
+  final List<Image> images;
+  final List<Parameter> parameters;
+  final SellingMode sellingMode;
 
-  Item(this.id, this.name, this.url);
+  Item(this.id, this.name, this.url, this.images, this.parameters,
+      this.sellingMode);
 
   Item.fromJson(Map<String, dynamic> json)
       : id = json['id'],
         name = json['name'],
-        url = json['url'];
+        url = json['url'],
+        images = (json['images'] as List)
+            .map((item) => Image.fromJson(item))
+            .toList(),
+        parameters = (json['parameters'] as List)
+            .map((item) => Parameter.fromJson(item))
+            .toList(),
+        sellingMode = SellingMode.fromJson(json['sellingMode']);
 }
