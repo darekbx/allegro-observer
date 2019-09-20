d5ae0e115a416637b08bccb39846728d6fb70263
Darek-Mac <darek.baranczuk@gmail.com>
2019-09-20 14:50:55 +0200

    Changed type of amount

--- lib/allegro/model/sellingmode.dart
+++ lib/allegro/model/sellingmode.dart
@@ -34,7 +34,7 @@ class Price {
   Price(this.amount, this.currency);
 
   Price.fromJson(Map<String, dynamic> json)
-      : amount = (json['amount'] as num).toString(),
+      : amount = json['amount'] + "",
         currency = json['currency'];
 
   @override
